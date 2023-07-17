1531d8eaf49ed01b7e39727f458f7ebb5190ec0e
train4aws <79434869+train4aws@users.noreply.github.com>
2023-07-17 18:58:27 -0400

    Update userdata-setup.sh

--- userdata-setup.sh
+++ userdata-setup.sh
@@ -48,3 +48,10 @@ chmod 700 get_helm.sh
 
 # Remove the installation script
 rm get_helm.sh
+
+# SSM Agent Installation
+mkdir /tmp/ssm
+cd /tmp/ssm
+wget https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/debian_amd64/amazon-ssm-agent.deb
+sudo dpkg -i amazon-ssm-agent.deb
+sudo systemctl enable amazon-ssm-agent
